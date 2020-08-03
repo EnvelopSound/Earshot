71e79dad828052429b0e329bb11179d45be9a1e6
Nadir Hamid <matrix.nad@gmail.com>
2020-08-03 20:13:06 +0000

    fix tester

--- tester/run-tests.sh
+++ tester/run-tests.sh
@@ -3,4 +3,4 @@
 set -e
 bash -c "ffmpeg -y -stream_loop -1 -i test.wav -af \"channelmap=channel_layout=hexadecagonal\" -c:a aac -ac 16 -b:a 2048k -f flv \"rtmp://nginx-rtmp:1935/live/stream1?token=${RTMP_AUTH_TOKEN}\" &"
 sleep 30
-curl --fail http://nginx-rtmp:80/stream1.mpd
+curl --fail http://nginx-rtmp:80/dash/stream1.mpd
