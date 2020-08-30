1d6db61dc2d9c93dec39181c099ede955ab9570e
Nadir Hamid <matrix.nad@gmail.com>
2020-08-30 20:07:59 +0000

    add 10s delay to tester

--- tester/run-tests.sh
+++ tester/run-tests.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # exit when any command fails
 set -e
+sleep 10
 bash -c "ffmpeg -y -stream_loop -1 -i test.wav -af \"channelmap=channel_layout=hexadecagonal\" -c:a aac -ac 16 -b:a 2048k -f flv \"rtmp://nginx-rtmp:1935/live/stream1?token=${RTMP_AUTH_TOKEN}\" &"
 sleep 30
 curl --fail http://nginx-rtmp:80/dash/stream1.mpd
