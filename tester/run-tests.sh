53e7a787302f4459bf638a373c8c410821e61f06
Nadir Hamid <matrix.nad@gmail.com>
2021-04-21 16:16:57 +0000

    Change shell scripts

--- tester/run-tests.sh
+++ tester/run-tests.sh
@@ -34,7 +34,8 @@ fi
 
 echo "AUTH test passed!"
 
-sleep 10
+echo "Testing media streaming"
+
 bash -c "ffmpeg -y -stream_loop -1 -i test.wav -af \"channelmap=channel_layout=hexadecagonal\" -c:a aac -ac 16 -b:a 2048k -f flv \"rtmp://nginx-rtmp:1935/live/stream1?token=${RTMP_AUTH_TOKEN}\" &"
 sleep 30
 echo "Testing HTTP"
